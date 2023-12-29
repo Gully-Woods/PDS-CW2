ce6700b2cca6e46c84eee5607e7709be0bfafb56
Gully-woods <woods.gulliver12@gmail.com>
2023-12-29 16:19:02 +0000

    some changes to code to tidy data set

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import geopandas as gp
 import matplotlib.pyplot as plt
 
@@ -8,3 +7,33 @@ print(gp_df)
 
 gp_df.plot(column="POP_5_YRS_AND_OLDER")
 plt.show()
+
+'''
+Ideas for tidying this data set:
+I can turn quantile into a one hot encoded problem where each quantile is turned into a number scale. 
+So for Health Disadvantage Quintile/HEALTH_DISADV_QUINTILE there are 5 rankings : most disadvantaged, lowest, midlle ...
+Lets call give these quantiles a number say 0,1,2,3,4 . 
+If this type of encoding works we can copy the method for all data 
+'''
+
+Quantile_cols=['HEALTH_DISADV_QUINTILE','SOCIOECON_DISADV_QUINTILE']
+for i in Quantile_cols:
+    gp_df[i].replace('Highest Equity Priority/Most Disadvantaged',0,inplace=True)
+    gp_df[i].replace('Lowest',4,inplace=True)
+    gp_df[i].replace('Middle',2,inplace=True)
+    gp_df[i].replace('Second Highest',1,inplace=True)
+    gp_df[i].replace('Second Lowest',3,inplace=True)
+    print(gp_df[i])
+
+
+'''so that worked for HEALTH_DISADV_QUINTILE let's try to do this for all the columns'''
+
+remaining_Quantile_cols=["RACE_ELL_ORIGINS_QUINTILE","COMPOSITE_QUINTILE"]
+for i in remaining_Quantile_cols:
+    gp_df[i].replace('Highest Equity Priority', 0, inplace=True)
+    gp_df[i].replace('Lowest', 4, inplace=True)
+    gp_df[i].replace('Middle', 2, inplace=True)
+    gp_df[i].replace('Second Highest', 1, inplace=True)
+    gp_df[i].replace('Second Lowest', 3, inplace=True)
+    print(gp_df[i])
+
