89b278462abdbb1e5b84bdca2c235998a93342db
Gully-woods <woods.gulliver12@gmail.com>
2024-01-05 15:15:57 +0000

    further analysis modifications

--- main.py
+++ main.py
@@ -12,33 +12,29 @@ gp_df=gp.read_file(gp_fp)
 gp_df.plot(color='white',edgecolor='black')
 plt.show()
 
-'''
-Ideas for tidying this data set:
-I can turn quantile into a number scale this will help with analysis later. 
-So for Health Disadvantage Quintile/HEALTH_DISADV_QUINTILE there are 5 rankings : most disadvantaged, lowest, midlle ...
-Lets call give these quantiles a number say 0,1,2,3,4 . 
-If this type of encoding works we can copy the method for all data 
-'''
+
+#Ideas for tidying this data set:
+#I can turn quantile into a number scale this will help with analysis later. 
+#So for Health Disadvantage Quintile/HEALTH_DISADV_QUINTILE there are 5 rankings : most disadvantaged, lowest, midlle ...
+#Lets call give these quantiles a number say 0,1,2,3,4 . 
+#If this type of encoding works we can copy the method for all data 
+
 
 Quantile_cols=['HEALTH_DISADV_QUINTILE','SOCIOECON_DISADV_QUINTILE']
 for i in Quantile_cols:
-    gp_df[i].replace('Highest Equity Priority/Most Disadvantaged',0,inplace=True)
-    gp_df[i].replace('Lowest',4,inplace=True)
-    gp_df[i].replace('Middle',2,inplace=True)
-    gp_df[i].replace('Second Highest',1,inplace=True)
-    gp_df[i].replace('Second Lowest',3,inplace=True)
+    dict={"Highest Equity Priority/Most Disadvantaged":0,"Second Highest":1,"Middle":2,"Second Lowest":3,
+               "Lowest":4}
+    gp_df[i]=gp_df[i].map(dict)
     print(gp_df[i])
 
 
-'''so that worked for HEALTH_DISADV_QUINTILE let's try to do this for all the columns'''
+#so that worked for HEALTH_DISADV_QUINTILE let's try to do this for all the columns
 
 remaining_Quantile_cols=["RACE_ELL_ORIGINS_QUINTILE","COMPOSITE_QUINTILE"]
 for i in remaining_Quantile_cols:
-    gp_df[i].replace('Highest Equity Priority', 0, inplace=True)
-    gp_df[i].replace('Lowest', 4, inplace=True)
-    gp_df[i].replace('Middle', 2, inplace=True)
-    gp_df[i].replace('Second Highest', 1, inplace=True)
-    gp_df[i].replace('Second Lowest', 3, inplace=True)
+    dict={"Highest Equity Priority":0 , "Second Highest":1, "Middle":2, "Second Lowest":3,
+                 "Lowest":4}
+    gp_df[i]=gp_df[i].map(dict)
     print(gp_df[i])
 
 '''
@@ -68,7 +64,7 @@ fig_1,axs_1=plt.subplots(nrows=2,ncols=2,figsize=(15,15))
 axs_1=axs_1.flatten()
 
 for i in list_int:
-    gp_df_clean.plot(ax=axs_1[(list_int.index(i))], column=i,cmap='hot_r',legend=True)
+    gp_df_clean.plot(ax=axs_1[(list_int.index(i))], column=i,cmap='hot_r',legend=True,edgecolor='black')
     axs_1[(list_int.index(i))].set_title(i)
     axs_1[(list_int.index(i))].axis('off')
 plt.show()
@@ -102,6 +98,40 @@ for i in list_int_min:
     mean_squared_error=np.mean(sqr_error)
     print('the mean squared error against the second order fit was', mean_squared_error, 'for the column',i)
 
+# investigating how wealth affects health disadvantage.
 
+wealth_list=["PTL_POP_UNDER200PCT_POVERTY", "HEALTH_DISADV_SCORE"]
+fig_3,axs_3=plt.subplots(nrows=2,ncols=2,figsize=(15,15))
+axs_3=axs_3.flatten()
+
+#1,2 maps
+#4th box plot
+box_df=gp_df_clean[["PTL_POP_UNDER200PCT_POVERTY", "HEALTH_DISADV_SCORE"]]
+box_melt_df=pd.melt(box_df)
+#3rd scatter plot
+
+for i in wealth_list:
+    gp_df_clean.plot(ax=axs_3[(wealth_list.index(i))], column=i, cmap='Blues', legend=True,edgecolor='black')
+    axs_3[(wealth_list.index(i))].set_title(i)
+    axs_3[(wealth_list.index(i))].axis('off')
+sns.regplot(ax=axs_3[2],fit_reg=True,x=wealth_list[0],y=wealth_list[1],data=gp_df_clean)
+axs_3[2].set_title("Scatter Plot")
+sns.boxplot(ax=axs_3[3],x='variable',y='value',data=box_melt_df,color=".8", linecolor="#137", linewidth=.75)
+axs_3[3].set_title("Box Plot")
+plt.show()
+
+sns.set(rc={'figure.figsize':(11.7,8.27)})
+AX=sns.scatterplot(x=wealth_list[0],y=wealth_list[1],data=gp_df_clean,
+                hue="RACE_ELL_ORIGINS_QUINTILE",size="RACE_ELL_ORIGINS_QUINTILE",
+                sizes=(50,150))
+legend_handles, _=AX.get_legend_handles_labels()
+AX.legend(legend_handles,["Highest Equity Priority", "Second Highest", "Middle", "Second Lowest",
+                 "Lowest"],bbox_to_anchor=(1,1),title="legend")
+plt.xlabel('Health Disadvantage Score')
+plt.ylabel('Percentage of population under 200% of poverty level')
+plt.show()
+
+axs_4=sns.swarmplot(data=gp_df_clean,x="RACE_ELL_ORIGINS_QUINTILE",y="SOCIOECON_DISADV_PERCENTILE",hue="HEALTH_DISADV_QUINTILE")
+plt.show()
 
 
